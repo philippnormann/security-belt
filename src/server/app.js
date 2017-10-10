e9331587bd02715105df692158ac09c381f8ab98
Thomas Klinger <Thomas.Klinger@otto.de>
2017-10-10 14:19:22 +0200

    refactor(server): Rename logger to morgan

--- src/server/app.js
+++ src/server/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const logger = require('morgan');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
@@ -21,9 +21,7 @@ app.set('views', path.join(__dirname, '..','client', 'views'));
 app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, '..', '..', 'public', 'favicon.ico')));
-app.use(logger('short', {
-  skip: (req, res) => res['_headers']['cache-control']
-}));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
