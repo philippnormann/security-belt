5a242d8b74017d8e8d4edf7082c17645fe9a4663
Thomas Klinger <Thomas.Klinger@otto.de>
2017-10-11 12:48:23 +0200

    refactor(team): Remove dependency on window location hash. Use data attributes instead

--- src/client/js/skillFilter.js
+++ src/client/js/skillFilter.js
@@ -1,14 +1,42 @@
 /* eslint-env browser */
 'use strict';
 
-function filterSkills() {
-  const color = window.location.hash.slice(1);
-  const beltSections = document.querySelectorAll('.card-grid');
-  beltSections.forEach((beltSection) => {
-    beltSection.style.display = beltSection.id ===`${color}-section` ? 'block' : 'none';
+const selectors = {
+  skillCard: '.skill-list__item',
+  rankTabs: '.rank-select',
+  rankTab: '.rank-select__tab',
+  teamInfo: '.team-info'
+};
+
+function displayCards({ rank }) {
+  document.querySelectorAll(selectors.skillCard).forEach(function(card) {
+    const { skillRank } = card.dataset;
+    if(rank === 'all') {
+      card.style.display = 'block';
+      return;
+    }
+    if(skillRank !== rank) {
+      card.style.display = 'none';
+    } else  {
+      card.style.display = 'block';
+    }
   });
 }
 
-window.addEventListener('hashchange', () => {
-  filterSkills();
-});
+function initTabbing() {
+  document.querySelectorAll(selectors.rankTab).forEach(function(tab) {
+    tab.addEventListener('click', function(e) {
+      displayCards(e.target.dataset);
+      const oldActive = document.querySelector('.rank-select__tab.mdc-tab--active');
+      if(oldActive) {
+        oldActive.classList.toggle('mdc-tab--active');
+      }
+      tab.classList.toggle('mdc-tab--active');
+    });
+  });
+}
+
+if(document.querySelector(selectors.teamInfo)) {
+  initTabbing();
+}
+
